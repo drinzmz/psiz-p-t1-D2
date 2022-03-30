ba1970694abd4c2b9327bf41c5a527b420081f7d
Arek <bajurski@hotmail.comgit config --global user.email bajurski@hotmail.comgit config --global user.email bajurski@hotmail.com>
2022-03-31 01:52:17 +0200

    "Task 2 resolved

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='psiz-p-t1-D2',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='arek',
+    author_email='',
+    description=''
+)
